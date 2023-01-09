7f0a1fd3193fe17b7a28629628e7a8c45eb59a0d
RogerCiv <rogeliocivantos@gmail.com>
2023-01-09 20:37:04 +0100

    update 9 enero v2

---
+++ Programacion/UD04/src/ejercicios/Ejercicio25/Triangulo.java
@@ -0,0 +1,24 @@
+package ejercicios.Ejercicio25;
+
+import java.awt.*;
+
+public class Triangulo {
+    private Point a;
+    private Point b;
+    private Point c;
+
+    public Triangulo(Point a, Point b, Point c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    /*
+     * El método getPuntoMedio devuelve las coordenadas del punto medio del segmento
+     * que une p1 con p2. Dicho punto medio está definido por estas coordenadas:
+     * ൬𝑝1. 𝑥 + 𝑝2. 𝑥
+     * 2 , 𝑝1. 𝑦 + 𝑝2. 𝑦
+     * 2 ൰
+     */
+    
+}

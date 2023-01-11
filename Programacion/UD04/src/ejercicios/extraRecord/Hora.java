2d63550d70106f81d4e0236394556a460ac5b403
RogerCiv <rogeliocivantos@gmail.com>
2023-01-11 19:14:25 +0100

    update 11 ene v2

--- Programacion/UD04/src/ejercicios/extraRecord/Hora.java
+++ Programacion/UD04/src/ejercicios/extraRecord/Hora.java
@@ -1,5 +1,68 @@
 package ejercicios.extraRecord;
 
-public class Hora {
-    
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+//poner protecciones
+public record Hora(int hora, int minuto, int segundo) {
+
+    public Hora {
+        // throw new UnsupportedOperationException("Metodo sin programar");
+        if (hora < 0 || hora > 23) {
+            throw new IllegalArgumentException("Hora incorrecta");
+        }
+        if (minuto > 59 || minuto < 0) {
+            throw new IllegalArgumentException("Minuto incorrecto");
+        }
+        if (segundo > 59 || segundo < 0) {
+            throw new IllegalArgumentException("segundo incorrecto");
+        }
+
+    }
+
+    // nos devuelve una nueva hora añadiendo los segundos que indicamos como
+    // parametro
+    public Hora añadirSegundos(int segundos) {
+        // throw new UnsupportedOperationException("Metodo sin programar");
+        /*
+         * int total = (hora*3600) + (minuto * 60) + segundo + segundos;
+         * int nuevaH = (total % 3600) / 60;
+         * int nuevoM = (total % 60) % 60;
+         * return new Hora(nuevaH, nuevoM, segundos);
+         */
+        LocalTime horaU = toLocalTime();
+        horaU = horaU.plusSeconds(segundos);
+        return new Hora(horaU.getHour(), horaU.getMinute(), horaU.getSecond());
+
+    }
+
+    // devuelve true si la hora es entre las 20:00 y las 8:00 dia siguiente.
+    public boolean esNoche() {
+        // throw new UnsupportedOperationException("Metodo sin programar");
+        LocalTime h = toLocalTime();
+        return h.isAfter(LocalTime.of(20, 0, 0)) || h.isBefore(LocalTime.of(8, 0, 0));
+    }
+
+    // devuelve la hora en formato americano. hora:minuto:segundo AM/PM hora entre 1
+    // y 12.
+    public String toString() {
+       // throw new UnsupportedOperationException("Metodo sin programar");
+        return toLocalTime().format(DateTimeFormatter.ofPattern("h:m:s"));
+
+    }
+    // convierte la hora en un objeto LocalTime
+
+    public LocalTime toLocalTime() {
+        // throw new UnsupportedOperationException("Metodo sin programar");
+        return LocalTime.of(hora, minuto, segundo);
+    }
+
+    // suma dos horas
+    public static Hora suma(Hora h1, Hora h2) {
+      // throw new UnsupportedOperationException("Metodo sin programar");
+        LocalTime horaFinal = h1.toLocalTime().plusHours(h2.hora()).plusMinutes(h2.minuto()).plusSeconds(h2.segundo());
+      //  Hora resultado = new Hora(horaFinal.getHour(),horaFinal.getMinute(),horaFinal.getSecond());
+        return new Hora(horaFinal.getHour(),horaFinal.getMinute(),horaFinal.getSecond());
+    }
+
 }
--- Programacion/UD04/src/ejercicios/extraRecord/testHora.java
+++ Programacion/UD04/src/ejercicios/extraRecord/testHora.java
@@ -1,12 +1,78 @@
 package ejercicios.extraRecord;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.time.LocalTime;
+
 import org.junit.Test;
+
 public class testHora {
-    
+
     public testHora() {
     }
 
     @Test
     public void testSomeMethod() {
-      
+
+    }
+
+    @Test
+    public void testHora1() {
+        //Crear una Hora (10:00) y comprobar que la hora es 10:00
+        Hora h = new Hora(10, 0, 0);
+        assertEquals(10, h.hora());
+        assertEquals(0, h.minuto());
+        assertEquals(0, h.segundo());
+    }
+
+    @Test
+    public void testHora2() {
+        // Añadir 30 segundos a una hora creada "12:25:15", nos tendria que devolver "12:25:45"
+        Hora h = new Hora(12, 25, 15);
+        Hora segundos = h.añadirSegundos(30);
+        assertEquals(12, segundos.hora());
+        assertEquals(25, segundos.minuto());
+        assertEquals(45, segundos.segundo());
+    }
+
+    @Test
+    public void testHora3() {
+        //Comprobar si es de noche o no con distintas horas. (Es de noche entre las 20:00 y las 8:00 )
+        Hora h = new Hora(19, 55,9);
+        assertFalse(h.esNoche());
+        Hora h2 = new Hora(21,30,45);
+        assertTrue(h2.esNoche());
+        Hora h3 = new Hora(8,0,1);
+        assertFalse(h3.esNoche());
+    }
+
+    @Test
+    public void testHora4() {
+        //Devolver una hora (16:20:30) en modo Americano (4:20:30)
+        Hora h = new Hora(16,20,30);
+        assertEquals("4:20:30", h.toString());
+    }
+
+    @Test
+    public void testHora5(){
+        //pasar Hora a LocalTime
+        Hora h = new Hora(12, 10, 30);
+        LocalTime l = h.toLocalTime();
+        assertEquals(12, l.getHour());
+        assertEquals(10, l.getMinute());
+        assertEquals(30, l.getSecond());
+    }
+
+    @Test
+    public void testHora6(){
+        //sumar dos horas 
+        Hora h = new Hora(13, 20, 13);
+        Hora h2 = new Hora(22, 40, 51);
+        Hora h3 = Hora.suma(h, h2);
+        assertEquals(12, h3.hora());
+        assertEquals(1,h3.minuto());
+        assertEquals(4, h3.segundo());
     }
 }

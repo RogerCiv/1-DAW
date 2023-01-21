d15ace3433bf2f75aecc35262a8ed1d51f700a0a
RogerCiv <rogeliocivantos@gmail.com>
2023-01-21 15:33:21 +0100

    update

--- Programacion/UD04/src/ejercicios/PROGRAMAREPASO/Programa.java
+++ Programacion/UD04/src/ejercicios/PROGRAMAREPASO/Programa.java
@@ -4,69 +4,64 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Programa {
+
     public static void main(String[] args) {
-      
+
         try {
-            CatalogoTitulos catalogo;  catalogo = new CatalogoTitulosArchivos("titulos.txt");
-       
+            CatalogoTitulos catalogo = new CatalogoTitulosArchivos("titulos.txt");
 
-        int opcion = 0;
-        while (opcion != 4) {
-            System.out.println("""
-                    1. Añadir título.
-                    2. Consultar todos los títulos.
-                    3. Cambiar estado del título.
-                    4. Salir.
-                    """);
+            int opcion = 0;
+            while (opcion != 4) {
+                System.out.println("""
+                                   
+                                   1. Añadir Titulo.
+                                   2. Consultar todos los titulos.
+                                   3. Cambiar estado del titulo.
+                                   4. Salir.
+                                   """);
 
-            opcion = new Scanner(System.in).nextInt();
-
-            switch (opcion) {
-                case 1 -> {
-                    System.out.println("Introduce un DNI: ");
-                    String dni = new Scanner(System.in).nextLine();
-                    System.out.println("Introduce un nombre: ");
-                    String nombre = new Scanner(System.in).nextLine();
-                    System.out.println("Introduce estudios: ");
-                    String estudios = new Scanner(System.in).nextLine();
-                    System.out.println("Introduce un Estado (SIN RECIBIR = 0, RECIBIDO =1 o RECOGIDO = 2): ");
-                    int estado = new Scanner(System.in).nextInt();
-                    catalogo.añadirTitulo(dni, nombre, estudios, Estado.values()[estado]);
-                }
-                case 2 -> {
-                    for (Titulo i : catalogo.getTitulos()) {
-                        System.out.println(i.toString());
+                opcion = new Scanner(System.in).nextInt();
+                switch (opcion) {
+                    case 1 -> {
+                        System.out.println("introduce DNI");
+                        String dni = new Scanner(System.in).nextLine();
+                        System.out.println("introudce nombre:");
+                        String nombre = new Scanner(System.in).nextLine();
+                        System.out.println("Introduce estudios");
+                        String estudios = new Scanner(System.in).nextLine();
+                        System.out.println("Introduce estado: (SIN_RECIBIR = 0, RECIBIDO = 1, RECOGIDO = 2)");
+                        int estado = new Scanner(System.in).nextInt();
+                        catalogo.añadirTitulo(dni, nombre, estudios, Estado.values()[estado]);
+                    }
+                    case 2 -> {
+                        for (Titulo i : catalogo.getTitulos()) {
+                            System.out.println(i.toString());
+                        }
+                    }
+                    case 3 -> {
+                         
+                        System.out.println("Introduce un dni para consultar");
+                        String dniConsultar = new Scanner(System.in).nextLine();
+                        Titulo ti = catalogo.getTitulo(dniConsultar);
+                        System.out.println("A que estado cambiar? (SIN_RECIBIR = 0, RECIBIDO = 1, RECOGIDO = 2)");
+                        int dniCambio = new Scanner(System.in).nextInt();
+                        ti.setEstado(Estado.values()[dniCambio]);
+                        catalogo.guardar();
+                        
+                    }
+                    case 4 -> {
+                        catalogo.guardar();
+                        break;
+                    }
+                    default -> {
+                        System.out.println("Error al elegir la opcion");
                     }
                 }
-                case 3 -> {
-                    System.out.println("Introduce un DNI para buscar sus titulos");
-                    String dni2 = new Scanner(System.in).nextLine();
-                    Titulo ti = catalogo.getTitulo(dni2);
-                    if (ti == null) {
-                        System.out.println("DNI No tiene titulos");
-                    } else {
-                        //esto meterlo arriba en el IF y este if cambiarlo por un try catch.
-                        System.out.println(
-                                "En que nuevo estado queremos poner el titulo SIN RECIBIR = 0, RECIBIDO =1 o RECOGIDO = 2):");
-                        int cambiarDNI = new Scanner(System.in).nextInt();
-                        ti.setEstado(Estado.values()[cambiarDNI]);
-                    }
 
-                }
-                case 4 -> {
-                    catalogo.guardar();
-                    break;
-                }
-                default -> {
-                    System.out.println("Opcion incorrecta, prueba de nuevo");
-                }
             }
 
-        }
-    } catch (IOException e) {
-        System.out.println("Error al abrir el archivo de títulos");
-        return;
+        } catch (IOException error) {
+            System.out.println("Error al cargar el archivo");
         }
     }
-  
 }
